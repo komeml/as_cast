991fef889a44766da3951f539c9204e61e89f895
komeml <58187672+komeml@users.noreply.github.com>
2026-07-01 12:49:48 +0900

    [feat] f32/f64へのキャスト機能を追加
    - `CastF32`、`CastF64`トレイトを定義
    - マクロで符号あり・符号なし整数・浮動小数点型に
      一括実装

---
+++ src/cast.rs
@@ -0,0 +1,41 @@
+use crate::{for_float, for_signed_scalars, for_unsigned_scalars};
+
+pub trait CastF32 {
+    fn cast_f32(self) -> f32;
+}
+
+pub trait CastF64 {
+    fn cast_f64(self) -> f64;
+}
+
+macro_rules! cast_f32 {
+    ($($type:ty), *) => {
+        $(
+        impl CastF32 for $type {
+            fn cast_f32(self) -> f32 {
+                self as f32
+            }
+        }
+        )*
+    };
+}
+
+macro_rules! cast_f64 {
+    ($($type:ty), *) => {
+        $(
+        impl CastF64 for $type {
+            fn cast_f64(self) -> f64 {
+                self as f64
+            }
+        }
+        )*
+    };
+}
+
+for_unsigned_scalars!(cast_f32);
+for_signed_scalars!(cast_f32);
+for_float!(cast_f32);
+
+for_unsigned_scalars!(cast_f64);
+for_signed_scalars!(cast_f64);
+for_float!(cast_f64);
