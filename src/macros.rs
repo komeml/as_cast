6de3133da5758ebb66d88e538aac91a9ac8684db
komeml <58187672+komeml@users.noreply.github.com>
2026-07-01 12:49:19 +0900

    [feat] 数値型スカラー用マクロを追加
    - 符号なし整数型 (u8〜usize) を列挙するマクロを追加
    - 符号あり整数型 (i8〜isize) を列挙するマクロを追加
    - 浮動小数点型 (f32, f64) を列挙するマクロを追加

---
+++ src/macros.rs
@@ -0,0 +1,20 @@
+#[macro_export]
+macro_rules! for_unsigned_scalars {
+    ($mac:ident) => {
+        $mac!(u8, u16, u32, u64, u128, usize);
+    };
+}
+
+#[macro_export]
+macro_rules! for_signed_scalars {
+    ($mac:ident) => {
+        $mac!(i8, i16, i32, i64, i128, isize);
+    };
+}
+
+#[macro_export]
+macro_rules! for_float {
+    ($mac:ident) => {
+        $mac!(f32, f64);
+    };
+}
